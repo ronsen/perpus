d5cbcff775d1806b38fc433d7e4a9914f46503e6
Ronsen <ronsen@startrek.or.id>
2024-06-06 12:15:42 +0700

    update

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,15 +5,31 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
-    protected static string $resource = UserResource::class;
+	protected static string $resource = UserResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
+	protected function getHeaderActions(): array
+	{
+		return [
+			Actions\DeleteAction::make(),
+		];
+	}
+
+	protected function handleRecordUpdate(Model $record, array $data): Model
+	{
+		$record->name = $data['name'];
+		$record->email = $data['email'];
+
+		if ($data['password']) {
+			$record->password = Hash::make($data['password']);
+		}
+
+		$record->update();
+
+		return $record;
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',  \App\Http\Controllers\HomeController::class)->name('home');
+Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
+
+Route::get('/login', function () {
+	return redirect()->route('home');
+})->name('login');
 
 Route::post('/login',  [\App\Http\Controllers\LoginController::class, 'store']);
